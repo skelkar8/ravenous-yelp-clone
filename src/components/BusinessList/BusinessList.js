223197e63d88cf9011e99299f90975249a60071b
Swapnil Kelkar <skelkar@purdue.edu>
2020-06-11 23:47:59 -0400

    2nd day

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import './BusinessList.css';
-import Business from '../Business/Business';
+import React from "react";
+import "./BusinessList.css";
+import Business from "../Business/Business";
 
 class BusinessList extends React.Component {
-    render(){
-        return (
-            <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="BusinessList">
+        {this.props.businesses.map(business => {
+          return <Business business={business} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default BusinessList;
